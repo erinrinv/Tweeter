748a378fc0b06a401f277eb30fd9001b698f70f1
Erin Von Gertzen <ianvon@shaw.ca>
2024-02-27 16:17:47 -0700

    refactored some functions in clients.js as well as created loadtweets and added timeago to the template generated as well as script in index.html

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -4,67 +4,85 @@
  * Reminder: Use (and do all your DOM work in) jQuery's document ready function
  */
 
-const data = [
-  {
-    "user": {
-      "name": "Newton",
-      "avatars": "https://i.imgur.com/73hZDYK.png",
-      "handle": "@SirIsaac"
-    },
-    "content": {
-      "text": "If I have seen further it is by standing on the shoulders of giants"
-    },
-    "created_at": 1461116232227
-  },
-  {
-    "user": {
-      "name": "Descartes",
-      "avatars": "https://i.imgur.com/nlhLi3I.png",
-      "handle": "@rd"
-    },
-    "content": {
-      "text": "Je pense, donc je suis"
-    },
-    "created_at": 1461113959088
-  }
-];
+const data = [];
 
 
 const renderTweets = function(tweets) {
-  // Empty the tweets container before rendering new tweets
   $('#tweets-container').empty();
-
-  // Loop through the tweets array
-  tweets.forEach(tweet => {
-    // Create a tweet element for each tweet object
+  for (let tweet of tweets) {
     const $tweet = createTweetElement(tweet);
-    // Append the tweet element to the tweets container
-    $('#tweets-container').prepend($tweet);
-  });
+    $('#tweets-container').append($tweet);
+  }
 };
 
 
-const createTweetElement = function(tweet) {
-  // Create HTML structure for tweet
-  const $tweet = $(`
-    <article class="tweet">
-      <header>
-        <img src="${tweet.user.avatars}" alt="Profile Picture">
-        <div class="tweet-header">
-          <h3>${tweet.user.name}</h3>
-          <p>${tweet.user.handle}</p>
+const createTweetElement = function(data) {
+  let $tweet = $(`
+<article class="tweet">
+      <header class="tweet-header">
+        <div class="user-profile">
+          <img class="user-icon" src="${tweetData.user.avatars}"></img> 
+          <h4 class="user-name">${tweetData.user.name}</h4>
+        </div>
+        <div>
+          <h4 class="user-handle">${tweetData.user.handle}</h4>
         </div>
       </header>
-      <p class="tweet-content">${tweet.content.text}</p>
-      <footer>
-        <span class="tweet-icon"><i class="fas fa-heart"></i> 0</span>
-        <span class="tweet-icon"><i class="fas fa-retweet"></i> 0</span>
-        <span class="tweet-icon"><i class="fas fa-comment"></i> 0</span>
+      <div class="tweet-text">
+        ${escape(tweetData.content.text)}
+      </div>
+      <footer class="tweet-footer">
+        <span class="tweet-date">${timeago.format(tweetData.created_at)}</span>
+        <div class="tweet-response">
+          <i class="fas fa-flag"></i>
+          <i class="fas fa-retweet"></i>
+          <i class="fas fa-heart"></i>
+        </div>
       </footer>
-    </article>
-  `);
-
+    </article>`);
   return $tweet;
 };
 
 //renderTweets(data);
+
+const loadTweets = function() {
+  $.get("/tweets/", function(newTweet) {
+    renderTweets(newTweet.reverse());
+  });
+};
+
+loadTweets();
+
+
+
+
+
+
+
+// Add event listener for form submission
+$(document).ready(function() {
+  $('form.tweetform').submit(function(event) {
+    // Prevent default form submission behavior
+    event.preventDefault();
+
+    // Serialize form data
+    const formData = $(this).serialize();
+
+    // Send POST request with AJAX
+    $.ajax({
+      method: 'POST',
+      url: '/tweets/',
+      data: formData
+    })
+    .then(function(response) {
+      // Optionally, you can handle the response from the server here
+      console.log('Tweet successfully submitted:', response);
+
+      // After successful submission, you may want to fetch and render tweets again
+      // renderTweets(response); // Assuming the response contains updated tweet data
+    })
+    .catch(function(error) {
+      console.error('Error submitting tweet:', error);
+    });
+  });
+});
